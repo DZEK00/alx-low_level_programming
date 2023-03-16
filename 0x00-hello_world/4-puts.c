67d96e90c6099b7b376117656d45e25b36388c0a
DZEK00 <zakarialyazale7@gmail.com>
2023-03-16 14:49:39 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 /**
- * The main function is the entry point of the program.
+  * The main function is the entry point of the program. It is responsible for
+ * initializing the program and executing the necessary tasks. In this program,
+ * the main function reads input from the user, performs some calculations, and
+ * displays the results to the screen. The program then exits after the user
+ * presses a key.
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
+/*This return value ends the program */
 return (0);
 }
