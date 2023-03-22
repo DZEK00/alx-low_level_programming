f445ac5c6794850e18fbd1f7a4f510443d2fd208
DZEK00 <zakarialyazale7@gmail.com>
2023-03-21 19:30:42 -0700

    add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -6,12 +6,15 @@
  *
  * @x: Takes integer type as input of the function
  *
+ * @y: Takes integer type as input of the function
+ *
  * Return: The result of adding two integers
  */
 
 int add(int x, int y)
 {
 	int result;
+
 	result = x + y;
 	return (result);
 }
