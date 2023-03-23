7652d4838e20d4d8bcea947bb4eee39b95586303
DZEK00 <zakarialyazale7@gmail.com>
2023-03-23 14:33:23 -0700

    2 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function that prints the
+ * numbers from 0 to 9
+ */
+
+void print_most_numbers(void)
+{
+        int i;
+
+        for (i = 0; i < 10; i++)
+        {
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(48 + i);
+		}
+        }
+                _putchar('\n');
+}
+
