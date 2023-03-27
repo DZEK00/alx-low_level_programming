eed7f26dcddb882ea88302db70fb420f29c0ea09
DZEK00 <zakarialyazale7@gmail.com>
2023-03-27 12:04:03 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _strlen - calculates the length of a string
+*
+* @s: input
+*
+* Return: length of the string
+*/
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (length != '\0')
+	{
+		length++;
+		s++;
+	return (length);
+	}
