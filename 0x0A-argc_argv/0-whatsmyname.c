632212f197bb345568ec838384d52092edb351f0
DZEK00 <zakarialyazale7@gmail.com>
2023-04-07 20:58:12 -0700

    argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - check the code
+*
+* @argc: argument
+* @argv: argument
+*
+* Return: Always 0.
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s", argv[i]);
+		printf("\n");
+	}
+	return (0);
+}
