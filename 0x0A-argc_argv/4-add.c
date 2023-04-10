1d821decd76b0f04a1fcafea528e7b4204c2218f
DZEK00 <zakarialyazale7@gmail.com>
2023-04-10 14:33:49 -0700

    argc_argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+* main - checks the code
+*
+* @argc: input
+* @argv: input
+*
+* Return: Always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int sum, num, i, j;
+	char *arg;
+
+	sum = 0;
+
+	if (argc == 1)
+	{
+ 		printf("0\n");
+		return (0);
+	}
+
+
+
+	for (i = 1; i < argc; i++)
+	{
+		arg = argv[i];
+		for (j = 0; arg[j] != '\0'; j++)
+		{ 
+			if (!isdigit(arg[j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		num = atoi(arg);
+		sum += num;
+	}
+
+		printf("%d\n", sum);
+		return (0);
+}
