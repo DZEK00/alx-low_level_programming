f6640645ce4eb60af22c17efa0dd1ba99be5bad2
DZEK00 <zakarialyazale7@gmail.com>
2023-04-20 05:22:02 -0700

    func point

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,40 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: Always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int i, bytes;
+	char *ptr = (char *) main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < bytes; i++)
+	{
+		printf("%02x", ptr[i] & 0xFF);
+		if (i != bytes - 1)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
