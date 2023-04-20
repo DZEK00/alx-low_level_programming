d291c0067ab1b0ac83e4567fcbb374e836d6a759
DZEK00 <zakarialyazale7@gmail.com>
2023-04-19 19:38:35 -0700

    func point

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+
+/**
+ * op_add - adds two ints
+ *
+ * @a: input
+ * @b: input
+ *
+ * Return: sum of a and b
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ * op_sub - difference of two ints
+ *
+ * @a: input
+ * @b: input
+ *
+ * Return: difference of a and b
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+/**
+ * op_mul - multiplies two ints
+ *
+ * @a: input
+ * @b: input
+ *
+ * Return: product of a and b
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+ * op_div - divides two ints
+ *
+ * @a: input
+ * @b: input
+ *
+ * Return: division of a and b
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+
+/**
+ * op_mod - remainder of the division between two ints
+ *
+ * @a: input
+ * @b: input
+ *
+ * Return: the remainder
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
