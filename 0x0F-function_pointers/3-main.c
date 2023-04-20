65630bb1be313e10d2c21edf04fbce67ed51cf64
DZEK00 <zakarialyazale7@gmail.com>
2023-04-19 20:11:34 -0700

    tt

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,44 +1,41 @@
 #include "3-calc.h"
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 
+#define ARGV(X, S) (argv[X][0] == S)
 /**
- * main - Program Entry
+ * main - entry point, This fuction perform a calulaction base on 2 numbers and
+ * the given operator.
+ * @argc: number of agruments
+ * @argv: the string value of the arguments.
  *
- * @argc: argument counter
- * @argv: argument vector
- *
- * Return: 0-on success
-*/
+ * Return: 0 (on Sucess)
+ */
 int main(int argc, char *argv[])
 {
-	int num1, num2;
-	char *operator;
+	int a_int, b_int, result;
+	char *op;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-	operator = argv[2];
-
-	if (get_op_func(operator) == NULL || operator[1] != '\0')
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
-	if ((*operator == 47 || *operator == 37) && num2 == 0)
+	a_int = atoi(argv[1]);
+	b_int = atoi(argv[3]);
+	op = argv[2];
+	if ((ARGV(2, '/') || ARGV(2, '%')) && b_int == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
-
-	printf("%d\n", get_op_func(operator)(num1, num2));
+	if (!(get_op_func(op)))
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	result = (get_op_func(op))(a_int, b_int);
+	printf("%d\n", result);
 
 	return (0);
 }
