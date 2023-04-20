d81ac22b9765baed7d735512dbbb20c841d72588
DZEK00 <zakarialyazale7@gmail.com>
2023-04-20 06:18:13 -0700

    var funcs

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+* sum_them_all - a function that returns the sum of all its parameters
+*
+* @n: number of args
+*
+* Return: sum of all args
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum;
+	unsigned int i;
+	va_list list;
+
+	sum = 0;
+
+	va_start(list, n);
+
+	if (n == 0)
+		return (0);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(list, int);
+	}
+
+	return (sum);
+}
