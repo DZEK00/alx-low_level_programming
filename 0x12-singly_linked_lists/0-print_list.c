b88595484718d3f4be0170b024199da479d28af1
DZEK00 <zakarialyazale7@gmail.com>
2023-04-27 16:11:58 -0700

    linked lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+#include "stdio.h"
+
+/**
+* print_list - prints all the elements of a list_t list.
+*
+* @h: input
+*
+* Return: number of chars printed
+*/
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 1;
+	const list_t *queue = h;
+
+	if (queue == NULL)
+		return (0);
+
+	while (queue->next != NULL)
+	{
+		if (queue->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", queue->len, queue->str);
+
+		count++;
+		queue = queue->next;
+	}
+	printf("[%d] %s\n", queue->len, queue->str);
+
+	return (count);
+}
