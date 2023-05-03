efa4f4b603ef4e6bcd6d042954d611072ea11092
DZEK00 <zakarialyazale7@gmail.com>
2023-05-02 18:22:07 -0700

    linked lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,22 +22,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new_node == NULL)
 		return (NULL);
 
+	new_node->n = n;
 	if (idx == 0)
 	{
-		new_node = malloc(sizeof(listint_t));
-        	if (new_node == NULL)
-            		return NULL;
-
-		new_node->n = n;
 		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
 	}
-
 	queue = *head;
 	for (i = 0; i < idx - 1; i++)
 	{
-		if  (queue == NULL)
+		if (queue == NULL)
 		{
 			free(new_node);
 			return (NULL);
@@ -47,4 +42,4 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->next = queue->next;
 	queue->next = new_node;
 	return (new_node);
-}	
+}
